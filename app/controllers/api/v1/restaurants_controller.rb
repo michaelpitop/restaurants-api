c4ad1d19338a5563ab8dc72b1de824dc854a501a
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-05-26 15:24:17 +0200

    .

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
-  before_action :set_restaurant, only: [ :show ]
+  before_action :set_restaurant, only: [ :show, :destroy ]
 
   def show
   end
@@ -8,10 +8,32 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
     @restaurants = policy_scope(Restaurant)
   end
 
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    head :no_content
+    # No need to create a `destroy.json.jbuilder` view
+  end
+
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
     authorize @restaurant  # For Pundit
+  end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
